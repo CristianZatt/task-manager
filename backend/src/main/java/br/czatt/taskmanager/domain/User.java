2928a04ace9710389f73d5d8977dcd95be37e10a
CristianZatt <cristian.zatt@gmail.com>
2018-09-25 13:24:47 -0300

    inicio de tentativa de login

--- backend/src/main/java/br/czatt/taskmanager/domain/User.java
+++ backend/src/main/java/br/czatt/taskmanager/domain/User.java
@@ -1,54 +1,70 @@
 package br.czatt.taskmanager.domain;
 
-import javax.persistence.*;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+ 
 @Entity
-@Table(name="users")
+@Table(name = "users", //
+        uniqueConstraints = { //
+                @UniqueConstraint(name = "USER_UC", columnNames = "name") })
 public class User {
-
-    // PrimaryKey
+    
+    // PrimaryKey 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    private String firstName;
-    private String lastName;
-
-    protected User() {}
-
-    public User(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
+ 
+    @Column(name = "name", length = 36, nullable = false)
+    private String name;
+ 
+    @Column(name = "encrytedPassword", length = 128, nullable = false)
+    private String encrytedPassword;
+ 
+    @Column(name = "enabled", length = 1, nullable = false)
+    private boolean enabled;
 
     @Override
     public String toString() {
         return String.format(
-                "User[id=%d, firstName='%s', lastName='%s']",
-                id, firstName, lastName);
+                "User[id=%d, name='%s', encrytedPassword='%s']",
+                id, name, encrytedPassword);
     }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public long getId() {
+ 
+    public Long getId() {
         return id;
     }
-
-    public void setId(long id) {
+ 
+    public void setId(Long id) {
         this.id = id;
     }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public String getEncrytedPassword() {
+        return encrytedPassword;
+    }
+ 
+    public void setEncrytedPassword(String encrytedPassword) {
+        this.encrytedPassword = encrytedPassword;
+    }
+ 
+    public boolean isEnabled() {
+        return enabled;
+    }
+ 
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+ 
 }
